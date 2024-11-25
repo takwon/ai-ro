cb22381562bf326e89346a270bb150b54e5d7343
takwon <52367088+takwon@users.noreply.github.com>
2024-11-25 18:57:34 +0900

    Add files via upload

---
+++ stl_test.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from langchain.chat_models import ChatOpenAI
+st.set_page_config(page_title="뭐든지 질문하세요~")
+st.title('뭐든지 질문하세요~')
+
+import os
+import openai
+
+openai.api_key = os.environ["OPENAI_API_KEY"]
+
+def generate_response(input_text): #llm이 답변 생성
+    llm = ChatOpenAI(temperature=0,
+                     model_name='gpt-4',
+                     )
+    st.info(llm.predict(input_text))
+
+with st.form('Question'):
+    text = st.text_area('질문 입력:', 'what types of text models does OpenAI provide?')
+    submitted = st.form_submit_button('보내기')
+    generate_response(text)
